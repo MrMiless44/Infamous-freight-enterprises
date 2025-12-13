5774a894c39fddce0f71feef32024fa76c6b1f0b
MR MILES <237955567+MrMiless44@users.noreply.github.com>
2025-12-13 15:16:48 +0000

    fix: remove conflicting .eslintrc.json from web package for flat config compatibility

---
+++ web/.next/server/pages/_app.js
@@ -0,0 +1,30 @@
+(() => {
+  var e = {};
+  ((e.id = 888),
+    (e.ids = [888]),
+    (e.modules = {
+      2017: (e, r, s) => {
+        "use strict";
+        (s.r(r), s.d(r, { default: () => i }));
+        var t = s(997);
+        function i({ Component: e, pageProps: r }) {
+          return t.jsx("div", {
+            style: {
+              fontFamily: "system-ui, -apple-system, BlinkMacSystemFont",
+            },
+            children: t.jsx(e, { ...r }),
+          });
+        }
+        s(8047);
+      },
+      8047: () => {},
+      997: (e) => {
+        "use strict";
+        e.exports = require("react/jsx-runtime");
+      },
+    }));
+  var r = require("../webpack-runtime.js");
+  r.C(e);
+  var s = r((r.s = 2017));
+  module.exports = s;
+})();
